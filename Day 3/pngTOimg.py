22d2a5f6abae855fc09a05ea8f8a2d6a90ca59da
thasvithu <thasvithu7@gmail.com>
2024-01-11 16:30:30 +0530

    Your commit message

--- Day 3/pngTOimg.py
+++ Day 3/pngTOimg.py
@@ -5,3 +5,10 @@ img = cv2.imread("photo.png") #Read an img
 cv2.imshow('show', img) #to diplay the image
 
 cv2.imwrite("photo.jpg", img)
+
+
+
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
+
+cv2.waitKey(10000)
